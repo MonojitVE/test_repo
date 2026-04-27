612db553c1b949b729f345a70b342f844f952007
Ankita banerjee <ankitabanerjee@virtualemployee.com>
2026-04-27 19:00:41 +0530

    fixed backend

--- Backend/utils/file_utils.py
+++
@@ -1,4 +0,0 @@
-def save_proposal(proposal_text, filename:str = "proposal_output.txt"):
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(proposal_text)
-    print(f"Proposal saved to {filename}")
